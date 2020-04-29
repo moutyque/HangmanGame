e41dd86a0ec42ffd71e09ea4abdab6aed8bb3879
Quentin <azfboom@gmail.com>
2020-04-29 11:50:29 +0200

    Bug fix :
    Change the way of dealing with the "A propose" menu. Replace the
    MenutItem to a Menu and use a MenueListener.

--- HangmanGame/src/main/java/org/hangman/views/menus/AboutMenue.java
+++ HangmanGame/src/main/java/org/hangman/views/menus/AboutMenue.java
@@ -1,13 +1,12 @@
 package org.hangman.views.menus;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JMenuItem;
+import javax.swing.JMenu;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
 
 import org.hangman.views.window.MainFrame;
 
-public class AboutMenue extends JMenuItem {
+public class AboutMenue extends JMenu {
 	private MainFrame frame =null;
 
 
@@ -18,12 +17,20 @@ public class AboutMenue extends JMenuItem {
 	
 	public void setFrame(MainFrame fram) {
 		this.frame = fram;		
-		this.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				frame.switchToAboutPanel();
-			}
-		});
+//		this.addActionListener(new ActionListener() {
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				frame.switchToAboutPanel();
+//			}
+//		});
+		this.addMenuListener(new MenuListener() {
+	        public void menuSelected(MenuEvent e) {
+	        	frame.switchToAboutPanel();
+	        }
+	        public void menuDeselected(MenuEvent e) {}
+
+	        public void menuCanceled(MenuEvent e) {}
+	    });
 	}
 
 
